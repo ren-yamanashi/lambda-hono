0ebef708d0d37ee8e997117e6a35e34ebaa8f1c5
ren-yamanashi <pinokionigg@icloud.com>
2024-06-22 13:34:18 +0900

    feat: update CDK Stack #5

--- lib/lambda-hono-stack.ts
+++ lib/lambda-hono-stack.ts
@@ -1,11 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import { Construct } from 'constructs';
+import { Construct } from "constructs";
 import * as lambda from 'aws-cdk-lib/aws-lambda'
 import * as apigw from 'aws-cdk-lib/aws-apigateway'
-import { IpAddresses, Vpc ,SubnetType} from 'aws-cdk-lib/aws-ec2';
+import { IpAddresses, Vpc ,SubnetType, SecurityGroup} from 'aws-cdk-lib/aws-ec2';
 import * as efs from 'aws-cdk-lib/aws-efs';
 import * as iam from 'aws-cdk-lib/aws-iam';
+
 export class LambdaHonoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, resourceName:string, props?: cdk.StackProps,) {
     super(scope, id, props);
@@ -25,9 +26,14 @@ export class LambdaHonoStack extends cdk.Stack {
       maxAzs: 2
     })
 
+    const securityGroup = new SecurityGroup(this, `SecurityGroup`, {
+      vpc,
+    });
+
     // NOTE: Definition of EFS
     const fileSystem = new efs.FileSystem(this, "FileSystem", {
       vpc,
+      fileSystemName: `${resourceName}-efs`,
       // files are not transitioned to infrequent access (IA) storage by default
       lifecyclePolicy: efs.LifecyclePolicy.AFTER_14_DAYS,
       performanceMode: efs.PerformanceMode.GENERAL_PURPOSE,
@@ -52,32 +58,81 @@ export class LambdaHonoStack extends cdk.Stack {
       },
     })
 
-    const lambdaFunction = new NodejsFunction(this, "lambda", {
+    const honoLambdaFn = new NodejsFunction(this, "lambda", {
       functionName: `${resourceName}-lambda`,
-      entry: 'lambda/index.ts',
+      entry: 'backend/index.ts',
       handler: 'handler',
+      memorySize: 256,
       runtime: lambda.Runtime.NODEJS_20_X,
+      timeout: cdk.Duration.seconds(15),
       vpc,
+      securityGroups: [securityGroup],
       filesystem: lambda.FileSystem.fromEfsAccessPoint(accessPoint, '/mnt/efs'),
       bundling: {
         forceDockerBundling: true, // lambda互換dockerコンテナ環境でTypeScriptのビルドが実行される
+        nodeModules: ["prisma", "@prisma/client"],
+        commandHooks: {
+          beforeInstall: (i,o) => [
+            // Copy prisma directory to Lambda code asset
+            // the directory must be placed on the same directory as your Lambda code
+            `cp -r ${i}/prisma ${o}`,
+          ],
+          beforeBundling: (i,o) => [],
+          afterBundling: (i,o) => [],
+        }
       },
       environment: {
         DATABASE_URL: 'file:/mnt/efs/prisma/dev.db',
-      }
+      },
+      depsLockFilePath: 'backend/package-lock.json',
     })
 
-    lambdaFunction.addFunctionUrl({
+    const migrateLambdaFn = new NodejsFunction(this, "MigrateLambda", {
+      functionName: `${resourceName}-migrate`,
+      entry: 'backend/migrate.ts',
+      handler: 'handler',
+      memorySize: 256,
+      runtime: lambda.Runtime.NODEJS_20_X,
+      timeout: cdk.Duration.seconds(15),
+      vpc,
+      securityGroups: [securityGroup],
+      filesystem: lambda.FileSystem.fromEfsAccessPoint(accessPoint, '/mnt/efs'),
+      bundling: {
+        forceDockerBundling: true, // lambda互換dockerコンテナ環境でTypeScriptのビルドが実行される
+        nodeModules: ["prisma", "@prisma/client"],
+        commandHooks: {
+          beforeInstall: (i,o) => [
+            // Copy prisma directory to Lambda code asset
+            // the directory must be placed on the same directory as your Lambda code
+            `cp -r ${i}/prisma ${o}`,
+          ],
+          beforeBundling: (i,o) => [],
+          afterBundling: (i,o) => [],
+        }
+      },
+      environment: {
+        DATABASE_URL: 'file:/mnt/efs/prisma/dev.db',
+      },
+      depsLockFilePath: 'backend/package-lock.json',
+    })
+
+    honoLambdaFn.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.NONE,
     })
 
     // NOTE: Define API Gateway
     new apigw.LambdaRestApi(this, 'myapi', {
-      handler: lambdaFunction
+      handler: honoLambdaFn,
+      restApiName: `${resourceName}-api`,
     })
 
-    lambdaFunction.addToRolePolicy(new iam.PolicyStatement({
-      actions: ["elasticfilesysten:*"],
+    honoLambdaFn.addToRolePolicy(new iam.PolicyStatement({
+      actions: ["elasticfilesystem:*"],
+      resources: [fileSystem.fileSystemArn]
+    }))
+
+    migrateLambdaFn.addToRolePolicy(new iam.PolicyStatement({
+      actions: ["elasticfilesystem:*"],
       resources: [fileSystem.fileSystemArn]
     }))
   }
