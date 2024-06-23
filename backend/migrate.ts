d32eb0f622f44292b10857c9886d7a82b5a487b3
Ren Yamanashi <96362223+ren-yamanashi@users.noreply.github.com>
2024-06-23 21:12:08 +0900

    feat: Update logging logic #13 (#14)

--- backend/migrate.ts
+++ backend/migrate.ts
@@ -15,17 +15,11 @@ export const handler: Handler = async () => {
         ['migrate', 'deploy'],
         (error, stdout) => {
           console.log(stdout);
-          if (error != null) {
-            console.log(`prisma migrate deploy exited with error ${error.message}`);
-            resolve(error.code ?? 1);
-          } else {
-            resolve(0);
-          }
+          resolve(error != null ? 1 : 1);
         },
       );
     });
-
-    if (exitCode != 0) throw Error(`command deploy failed with exit code ${exitCode}`);
+    if (exitCode) throw Error(`Migration failed with exit code ${exitCode}.`);
   } catch (error) {
     console.error(error);
     throw error;
